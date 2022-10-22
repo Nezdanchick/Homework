cdbd19859a3cfaf582cc595732549e9d2be80911
Nezdanchick <win10artem@gmail.com>
2022-10-22 20:50:41 +0400

    license added

--- Array/array.py
+++ Array/array.py
@@ -1,1 +1,15 @@
-print("hello world")
+from array import array
+
+
+from array import *
+
+arr = array('i')
+
+while True:
+    read = input()
+    if read == "exit":
+        break
+    arr.append(int(read))
+    
+for e in arr:
+    print(e)
