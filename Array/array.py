43e151d98e2c15e51d5d984cd51b65c1332d4c86
Nezdanchick <win10artem@gmail.com>
2022-10-22 21:26:45 +0400

    work 1 done

--- Array/array.py
+++ Array/array.py
@@ -1,15 +1,28 @@
-from array import array
-
-
 from array import *
 
-arr = array('i')
+_array = array('i') # i = int = integer
+ 
+_array = input().split(',') # split() slice string into an array of strings
+# массив = array
 
-while True:
-    read = input()
-    if read == "exit":
-        break
-    arr.append(int(read))
+i = 0 # iterator
+for e in _array:   # for each element in array
+
+    isNull = False # for clear strings
+
+    # 3)    
+    if e.strip() == "": # strip() deletes all spaces and tabs from string
+        print("пустой элемент -", i + 1, '\n')
+        isNull = True
+
+    if isNull == False:
+        # 1)
+        if (i % 2) == 1:
+            print("четный элемент -", e, '\n')
+
+        # 2)
+        if (int(e) % 3) != 0:
+            print("не кратное 3-м -", e, '\n')
     
-for e in arr:
-    print(e)
+    # i increase
+    i += 1
